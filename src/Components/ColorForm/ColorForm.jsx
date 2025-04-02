bd7832e3034fc2c0ab3a150c94632bfa2a92334d
Angela Capillo <hello@angelcapillo.com>
2025-04-02 14:21:10 +0200

    Add functionality to handle submit event

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,13 +1,27 @@
 import ColorInput from "../ColorInput/ColorInput";
 
-export default function ColorForm() {
+export default function ColorForm({ onAddEntry }) {
     
     const defaultValues = {role: "Primary color", hex: "#663399", contrastText: "#F0E8F7"};
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        const form = new FormData(event.target);
+        const data = Object.fromEntries(form);
+
+        console.log(data)
+    
+        //onAddEntry({role: data.role,})
+    
+        event.target.reset();
+        event.target.elements.role.focus();
+      }
+
+
   return (
-    <form >
+    <form onSubmit={handleSubmit}>
         <label htmlFor="role">Role</label>
-        <input type="text" id="role" name="role" value="Primary Color" />
+        <input type="text" id="role" name="role" defaultValue={defaultValues.role} />
         <label htmlFor="hex">Hex</label>
         <ColorInput name="hex" defaultValue={defaultValues.hex}></ColorInput>
         <label htmlFor="contrastText">Contrast text</label>
