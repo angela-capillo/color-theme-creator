c298dc25f809047d8c48c264790882fd483cafed
Angela Capillo <hello@angelcapillo.com>
2025-04-03 13:54:26 +0200

    Create CopyToClipboardButton component

---
+++ src/Components/CopyToClipboardButton/CopyToClipboardButton.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./CopyToClipboardButton.css";
+
+export default function CopyToClipboardButton() {
+    
+
+    return (
+        <button type="button">Copy</button>
+);
+}
